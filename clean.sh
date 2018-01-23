2fe4d20bd353a0129991a4290c8ff687efd60b6e
[szren] <[2528751462@qq.com]>
2018-01-23 23:18:09 +0800

    add helloworld

---
+++ clean.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+root_dir=$(cd `dirname $0`; pwd)
+# echo "root_dir:" ${root_dir}
+
+clean_dir=("helloworld" "./MultiplayerBook/")
+
+clean_len=${#clean_dir[@]}
+
+for ((i=0;i<${clean_len};i++))
+do
+	clear_sub=${clean_dir[$i]}
+	cd ${clear_sub}
+    echo `pwd`
+    make clean
+    cd ${root_dir};
+done
+cd $rootdir
+

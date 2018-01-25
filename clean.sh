651fe99bf62baa7f7e1092bc88a812dbf9df15ee
[szren] <[2528751462@qq.com]>
2018-01-25 10:55:35 +0800

    add GeneralHashFunctions

--- clean.sh
+++ clean.sh
@@ -2,7 +2,8 @@
 root_dir=$(cd `dirname $0`; pwd)
 # echo "root_dir:" ${root_dir}
 
-clean_dir=("helloworld" "./MultiplayerBook/" "./3rd/templog/")
+clean_dir=( "helloworld" "./MultiplayerBook/" "./3rd/templog/"
+            "GeneralHashFunctions_-_C" "GeneralHashFunctions_-_CPP")
 
 clean_len=${#clean_dir[@]}
 
@@ -10,9 +11,10 @@ for ((i=0;i<${clean_len};i++))
 do
 	clear_sub=${clean_dir[$i]}
 	cd ${clear_sub}
+    echo ""
     echo `pwd`
     make clean
-    cd ${root_dir};
+    cd ${root_dir}
 done
 cd $rootdir
 

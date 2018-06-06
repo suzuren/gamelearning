63ae0644b1304607a0ac096cec7fac4a8529b8f1
[szren] <[2528751462@qq.com]>
2018-06-06 20:12:16 +0800

    lua

---
+++ lua/test_loop.lua
@@ -0,0 +1,28 @@
+
+local printr = require "print_r"
+
+local tempTable = {11,22,33}
+
+print("for 1 3 -----------------------------------------------------")
+
+for i=1, 3 do
+	print(string.format("i:%d,table:%d",i,tempTable[i]))
+end
+
+print("for in pairs ------------------------------------------------")
+
+for i, v in pairs(tempTable) do
+	print(string.format("i:%d,table:%d",i,v))
+end
+
+
+print("protocalNo, protocalObj---------------------------------------")
+
+
+local protocalNo, protocalObj = 20200, {rate=300}
+
+print("protocalNo:",protocalNo)
+printr(protocalObj)
+
+
+print("-------------------------------------------------------------")

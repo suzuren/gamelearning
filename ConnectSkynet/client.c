fd83fd51c48344ecd137b2f06b286e210cd5fff1
[szren] <[2528751462@qq.com]>
2018-06-15 18:51:29 +0800

    update skynetConnect

--- ConnectSkynet/client.c
+++ ConnectSkynet/client.c
@@ -2,9 +2,169 @@
 #include "pbc/pbc.h"
 #include "readfile.h"
 
+#include <arpa/inet.h>
+
+#include <stdio.h>
+#include <stdbool.h>
+
+
+static struct pbc_wmessage * test_s2c_login_wmessage(struct pbc_env * env);
+static void test_s2c_login_rmessage(struct pbc_env *env, struct pbc_slice *slice);
+static struct pbc_wmessage * test_c2s_login_wmessage(struct pbc_env * env);
+static void test_c2s_login_rmessage(struct pbc_env *env, struct pbc_slice *slice);
+static bool CheckStorageMode();
+
+
+
+int main(int argc, char const *argv[])
+{
+	int client_fd = socket_connect(IPADDRESS, PORT);
+	printf("socket_connect client_fd:%d\n", client_fd);
+
+	
+	struct pbc_slice slice;
+	read_file("loginServer.login.c2s.pb", &slice);
+	if (slice.buffer == NULL)
+	{
+		printf("read_file error\n");
+		return 1;
+	}
+	struct pbc_env * env = pbc_new();
+	int ret = pbc_register(env, &slice);
+	if (ret)
+	{
+		printf("Error : %s", pbc_error(env));
+		return 1;
+	}
+	free(slice.buffer);
+	//printf("pbc_wmessage_buffer start\n");
+
+	struct pbc_wmessage *msg = test_c2s_login_wmessage(env);
+	pbc_wmessage_buffer(msg, &slice);
+
+	printf("test_wmessage success\n");
+	test_c2s_login_rmessage(env, &slice);
+	printf("test_rmessage success\n");
+	pbc_wmessage_delete(msg);
+	pbc_delete(env);
+
+	//int iCount = 0;
+
+	//struct heard{
+	//	unsigned short size;
+	//};
+	//struct heard * taghead = (struct heard *)write_buf;
+	
+	//int heardSize = sizeof(struct heard);
+
+	//printf("1 pack_size:%d,heardSize:%d - %d\n", pack_size, heardSize, taghead->size);
+
+	//if (CheckStorageMode())
+	//{
+	//	//taghead->size = pack_size;
+	//	memcpy(write_buf , &pack_size, sizeof(pack_size));
+	//}
+	//else
+	//{
+	//	pack_size = htons(pack_size);
+	//	memcpy(write_buf, &pack_size, sizeof(pack_size));
+	//}
+
+	//printf("2 pack_size:%d,heardSize:%d - %d\n", pack_size, heardSize, taghead->size);
+
+
+	char write_buf[65535] = { 0 };
+
+	unsigned int protoNo = 0x000100;
+	unsigned short pack_size = 4 + slice.len;
+	write_buf[0] = (pack_size >> 8) & 0xff;
+	write_buf[1] = pack_size & 0xff;
+	write_buf[2] = 0;
+	write_buf[3] = (protoNo >> 16) & 0xff;
+	write_buf[4] = (protoNo >> 8) & 0xff;
+	write_buf[5] = protoNo & 0xff;
+
+	memcpy(write_buf + 6, slice.buffer, slice.len);
+	int total_len_send = 0;
+
+	int k = 0;
+	for (int i = 3; i<pack_size + 2; i++)
+	{
+		k += ((unsigned char)write_buf[i]);
+		k &= 0xff;
+	}
+
+	printf("pack_size:%d,k:%d\n", pack_size, k);
+
+	write_buf[2] = k;
+
+	pack_size = 6 + slice.len;
+
+	for (; pack_size>0;)
+	{
+		int len_buff = strlen(write_buf);
+		int len_send = write(client_fd, write_buf + total_len_send, pack_size);
+
+		if (len_send > 0)
+		{
+			pack_size -= len_send;
+			total_len_send += len_send;
+
+			printf("%s len_buff:%d,len_send:%d,pack_size:%d,total_len_send:%d,buf:%s\n", getStrTime(), len_buff, len_send, pack_size, total_len_send, write_buf);
+
+		}
+	}
+
+
+	while (true)
+	{
+		//memset(write_buf, 0, sizeof(write_buf));
+		//sprintf(write_buf, "%s %d\n", "hello world", iCount++);
+
+		//int len_buff = strlen(write_buf);
+		//int len_send = write(client_fd, write_buf, len_buff);
+		//if (len_buff != len_send)
+		//{
+		//	printf("send buf error\n");
+		//}
+
+		//printf("%s len_buff:%d,len_send:%d,buf:%s\n", getStrTime(), len_buff, len_send, write_buf);
+
+		char read_buf[65535] = { 0 };
+		int nread = read(client_fd, read_buf, 65535);
+		if (nread == 0)
+		{
+			close(client_fd);
+			break;
+		}
+		else if (nread < 0 && (errno == EAGAIN || errno == EWOULDBLOCK || errno == EINTR))
+		{
+			continue;
+		}
+		else
+		{
+			printf("recv - len:%ld,buf:%s.\n", strlen(read_buf), read_buf);
+		}
+		sleep(1);
+	}
+
+
+}
+
+
+
+
+bool CheckStorageMode()
+{
+	int iFlag = 0x12345678;
+	char *cByte = (char *)&iFlag;
+	return(*cByte == 0x12);
+}
+
+
 
 static struct pbc_wmessage *
-test_wmessage(struct pbc_env * env)
+test_s2c_login_wmessage(struct pbc_env * env)
 {
 	struct pbc_wmessage* w_msg = pbc_wmessage_new(env, "loginServer.login.s2c.Login");
 
@@ -30,7 +190,7 @@ test_wmessage(struct pbc_env * env)
 }
 
 static void
-test_rmessage(struct pbc_env *env, struct pbc_slice *slice)
+test_s2c_login_rmessage(struct pbc_env *env, struct pbc_slice *slice)
 {
 	struct pbc_rmessage * r_msg = pbc_rmessage_new(env, "loginServer.login.s2c.Login", slice);
 	if (r_msg == NULL)
@@ -63,94 +223,48 @@ test_rmessage(struct pbc_env *env, struct pbc_slice *slice)
 	const char * pplatformFace = pbc_rmessage_string(pdata, "platformFace", 0, NULL);
 	int isChangeNickName = pbc_rmessage_integer(pdata, "isChangeNickName", 0, NULL);
 
-	printf("test_rmessage - userID:%d,gameID:%d,faceID:%d,gender:%d, pnickName:%s, isRegister:%d, memberOrder:%d, psignature:%s, pplatformFace:%s, isChangeNickName:%d\n",userID, gameID, faceID, gender, pnickName, isRegister, memberOrder, psignature, pplatformFace, isChangeNickName);
+	printf("test_rmessage - userID:%d,gameID:%d,faceID:%d,gender:%d, pnickName:%s, isRegister:%d, memberOrder:%d, psignature:%s, pplatformFace:%s, isChangeNickName:%d\n", userID, gameID, faceID, gender, pnickName, isRegister, memberOrder, psignature, pplatformFace, isChangeNickName);
 
 }
 
 
-int main(int argc, char const *argv[])
+static struct pbc_wmessage *
+test_c2s_login_wmessage(struct pbc_env * env)
 {
-	int client_fd = socket_connect(IPADDRESS, PORT);
-	printf("socket_connect client_fd:%d\n", client_fd);
+	struct pbc_wmessage* w_msg = pbc_wmessage_new(env, "loginServer.login.c2s.Login");
 
-	
-	struct pbc_slice slice;
-	read_file("loginServer.login.s2c.pb", &slice);
-	if (slice.buffer == NULL)
-	{
-		printf("read_file error\n");
-		return 1;
-	}
-	struct pbc_env * env = pbc_new();
-	int ret = pbc_register(env, &slice);
-	if (ret)
+	pbc_wmessage_string(w_msg, "session", "session test", -1);
+	pbc_wmessage_string(w_msg, "nickName", "alice", -1);
+	pbc_wmessage_string(w_msg, "machineID", "HYUInyuijh6", -1);
+	pbc_wmessage_integer(w_msg, "kindID", 200, 0);
+	pbc_wmessage_integer(w_msg, "scoreTag", 1, 0);
+	pbc_wmessage_integer(w_msg, "appID", 201, 0);
+	pbc_wmessage_string(w_msg, "appChannel", "appChannel test", -1);
+	pbc_wmessage_string(w_msg, "appVersion", "appVersion test", -1);
+
+	return w_msg;
+}
+
+static void
+test_c2s_login_rmessage(struct pbc_env *env, struct pbc_slice *slice)
+{
+	struct pbc_rmessage * r_msg = pbc_rmessage_new(env, "loginServer.login.c2s.Login", slice);
+	if (r_msg == NULL)
 	{
 		printf("Error : %s", pbc_error(env));
-		return 1;
-	}
-	free(slice.buffer);
-	//printf("pbc_wmessage_buffer start\n");
-
-	struct pbc_wmessage *msg = test_wmessage(env);
-	pbc_wmessage_buffer(msg, &slice);
-
-	printf("test_wmessage success\n");
-
-	test_rmessage(env, &slice);
-
-	printf("test_rmessage success\n");
-
-	pbc_wmessage_delete(msg);
-	pbc_delete(env);
-
-	//int iCount = 0;
-	char write_buf[65535] = { 0 };
-
-	unsigned int protoNo = 0x000100;
-
-	write_buf[1] = (protoNo >> 16) & 0xff;
-	write_buf[2] = (protoNo >> 8) & 0xff;
-	write_buf[3] = protoNo & 0xff;
-
-	memcpy(write_buf + 4, slice.buffer, slice.len);
-	int pack_size = 4 + slice.len;
-
-	printf("pack_size:%d\n", pack_size);
-
-	while (client_fd != -1)
-	{
-		//memset(write_buf, 0, sizeof(write_buf));
-		//sprintf(write_buf, "%s %d\n", "hello world", iCount++);
-
-		int len_buff = strlen(write_buf);
-		int len_send = write(client_fd, write_buf, len_buff);
-		if (len_buff != len_send)
-		{
-			printf("send buf error\n");
-		}
-
-		printf("%s len_buff:%d,len_send:%d,buf:%s\n", getStrTime(), len_buff, len_send, write_buf);
-
-		char read_buf[65535] = { 0 };
-		int nread = read(client_fd, read_buf, 65535);
-		if (nread == 0)
-		{
-			close(client_fd);
-			break;
-		}
-		else if (nread < 0 && (errno == EAGAIN || errno == EWOULDBLOCK || errno == EINTR))
-		{
-			continue;
-		}
-		else
-		{
-			//printf("recv - len:%d,buf:%s.\n", strlen(read_buf), read_buf);
-		}
-		sleep(1);
+		return;
 	}
 
+	const char * psession = pbc_rmessage_string(r_msg, "session", 0, NULL);
+	const char * pnickName = pbc_rmessage_string(r_msg, "nickName", 0, NULL);
+	const char * pmachineID = pbc_rmessage_string(r_msg, "machineID", 0, NULL);
+	int kindID = pbc_rmessage_integer(r_msg, "kindID", 0, NULL);
+	int scoreTag = pbc_rmessage_integer(r_msg, "scoreTag", 0, NULL);
+	int appID = pbc_rmessage_integer(r_msg, "appID", 0, NULL);
+	const char * pappChannel = pbc_rmessage_string(r_msg, "appChannel", 0, NULL);
+	const char * pappVersion = pbc_rmessage_string(r_msg, "appVersion", 0, NULL);
+
+	printf("test_rmessage - psession:%s, pnickName:%s, pmachineID:%s, kindID:%d, scoreTag:%d, appID:%d, pappChannel:%s, pappVersion:%s\n",
+		psession, pnickName, pmachineID, kindID, scoreTag, appID, pappChannel, pappVersion);
 
 }
-
-
-
---
+++ big_little_endian/main.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+#include <arpa/inet.h>
+// return : true - big endian , false - little endian
+
+bool CheckStorageMode()
+{
+	int iFlag = 0x12345678;
+	char *cByte = (char *)&iFlag;
+	return(*cByte == 0x12);
+}
+
+bool CheckStorageModeEx()
+{
+	int iFlag = 0x12345678;
+	iFlag = htons(iFlag);
+	char *cByte = (char *)&iFlag;
+	return(*cByte == 0x12);
+}
+
+int main(int argc, const char** argv)
+{
+	bool bStorageMode = CheckStorageModeEx();
+	printf("hello world - bStorageMode:%d\n", bStorageMode);
+	return 0;
+}
+

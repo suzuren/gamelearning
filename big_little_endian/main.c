1447395f0f6765e00fa3e572ac401ee7566b8cf9
[szren] <[2528751462@qq.com]>
2018-06-20 14:16:58 +0800

    add big stogre

--- big_little_endian/main.c
+++ big_little_endian/main.c
@@ -1,6 +1,7 @@
 
 #include <stdio.h>
 #include <arpa/inet.h>
+
 // return : true - big endian , false - little endian
 
 bool CheckStorageMode()
@@ -18,21 +19,45 @@ bool CheckStorageModeEx()
 	return(*cByte == 0x12);
 }
 
+int GetPacketLen(const char * data)
+{
+	static unsigned char buffer[2];
+	buffer[0] = 0;
+	buffer[1] = 0;
+	buffer[0] = data[0];
+	buffer[1] = data[1];
+	int depack_size = (int)buffer[0] << 8 | (int)buffer[1];
+
+	printf("GetPacketLen - pack_size:%d,buffer:%d,%d\n", depack_size, buffer[0], buffer[1]);
+
+	return depack_size;
+}
+
 void BigLittleStorage()
 {
 	// 两个字节最大能表示65535
-	unsigned char buffer[2] = { 0 };
+	char buffer[2] = { 0 };
 	unsigned short pack_size = 65535;
 	// 按照大端存进buffer
 	buffer[0] = (pack_size >> 8) & 0xff;
 	buffer[1] = pack_size & 0xff;
-	printf("big storage -pack_size:%d,buffer:%d,%d\n", pack_size,buffer[0],buffer[1]);
+	printf("big storage - pack_size:%d,buffer:%d,%d\n", pack_size,buffer[0],buffer[1]);
 
-	unsigned int depack_size = (unsigned int)buffer[0] << 8 | (unsigned int)buffer[1];
+	static unsigned char data[2];
+	data[0] = 0;
+	data[1] = 0;
+
+	data[0] = buffer[0];
+	data[1] = buffer[1];
+
+	unsigned int depack_size = (unsigned int)data[0] << 8 | (unsigned int)data[1];
 
 	printf("big storage decode - depack_size:%d\n", depack_size);
 
+	GetPacketLen(buffer);
 }
+
+
 
 int main(int argc, const char** argv)
 {

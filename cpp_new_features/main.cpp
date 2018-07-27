a1b0b48f0e12a794c21ff305ce920883b91b4401
szren <2528751462@qq.com>
2018-07-28 00:33:58 +0800

    cpp_new_features

---
+++ cpp_new_features/main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string>
+using namespace std;
+
+
+int main(int argc, const char** argv)
+{
+	printf("hello world! test cpp new features ... \n");
+	
+
+	return 0;
+}
+
+
+
+

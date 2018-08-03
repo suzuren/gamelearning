a5d1116fb1ea258cc27d1b0065aec3d07fd40d29
[szren] <[2528751462@qq.com]>
2018-08-03 16:36:30 +0800

    add lib makefile

---
+++ test_lib/testadd.c
@@ -0,0 +1,8 @@
+
+
+#include "testadd.h"
+
+int test_add_func(int a,int b)
+{
+    return a+b;
+}
---
+++ test_lib/testadd.h
@@ -0,0 +1,10 @@
+
+
+#ifndef __TEST_ADD_H__
+#define __TEST_ADD_H__
+
+int test_add_func(int a,int b);
+
+
+
+#endif

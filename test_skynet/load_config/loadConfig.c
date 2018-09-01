94df83d410b9b97827498f02089d727e5148b209
szren <2528751462@qq.com>
2018-09-02 01:54:14 +0800

    add 3rd/lua  and test_skynet and sub test_skynet/load_config project

---
+++ test_skynet/load_config/loadConfig.c
@@ -0,0 +1,232 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+#include <signal.h>
+#include <assert.h>
+
+#include "spinlock.h"
+
+//--------------------------------------------------------------------------
+
+#define skynet_malloc malloc
+
+//--------------------------------------------------------------------------
+
+
+struct skynet_config {
+	int thread;
+	int harbor;
+	int profile;
+	const char * daemon;
+	const char * module_path;
+	const char * bootstrap;
+	const char * logger;
+	const char * logservice;
+};
+
+
+struct skynet_env {
+	struct spinlock lock;
+	lua_State *L;
+};
+
+static struct skynet_env *E = NULL;
+
+//--------------------------------------------------------------------------
+
+const char * 
+skynet_getenv(const char *key) {
+	SPIN_LOCK(E)
+
+	lua_State *L = E->L;
+	
+	lua_getglobal(L, key);
+	const char * result = lua_tostring(L, -1);
+	lua_pop(L, 1);
+
+	SPIN_UNLOCK(E)
+
+	return result;
+}
+
+void 
+skynet_setenv(const char *key, const char *value) {
+	SPIN_LOCK(E)
+	
+	lua_State *L = E->L;
+	lua_getglobal(L, key);
+	assert(lua_isnil(L, -1));
+	lua_pop(L,1);
+	lua_pushstring(L,value);
+	lua_setglobal(L,key);
+
+	SPIN_UNLOCK(E)
+}
+
+void
+skynet_env_init() {
+	E = skynet_malloc(sizeof(*E));
+	SPIN_INIT(E)
+	E->L = luaL_newstate();
+}
+
+//--------------------------------------------------------------------------
+
+static int
+optint(const char *key, int opt) {
+	const char * str = skynet_getenv(key);
+	if (str == NULL) {
+		char tmp[20];
+		sprintf(tmp,"%d",opt);
+		skynet_setenv(key, tmp);
+		return opt;
+	}
+	return strtol(str, NULL, 10);
+}
+
+static int
+optboolean(const char *key, int opt) {
+	const char * str = skynet_getenv(key);
+	if (str == NULL) {
+		skynet_setenv(key, opt ? "true" : "false");
+		return opt;
+	}
+	return strcmp(str,"true")==0;
+}
+
+static const char *
+optstring(const char *key,const char * opt) {
+	const char * str = skynet_getenv(key);
+	if (str == NULL) {
+		if (opt) {
+			skynet_setenv(key, opt);
+			opt = skynet_getenv(key);
+		}
+		return opt;
+	}
+	return str;
+}
+
+static void
+_init_env(lua_State *L) {
+	lua_pushnil(L);  /* first key */
+	while (lua_next(L, -2) != 0) {
+		int keyt = lua_type(L, -2);
+		if (keyt != LUA_TSTRING) {
+			fprintf(stderr, "Invalid config table\n");
+			exit(1);
+		}
+		const char * key = lua_tostring(L,-2);
+		if (lua_type(L,-1) == LUA_TBOOLEAN) {
+			int b = lua_toboolean(L,-1);
+			skynet_setenv(key,b ? "true" : "false" );
+		} else {
+			const char * value = lua_tostring(L,-1);
+			if (value == NULL) {
+				fprintf(stderr, "Invalid config table key = %s\n", key);
+				exit(1);
+			}
+			skynet_setenv(key,value);
+		}
+		lua_pop(L,1);
+	}
+	lua_pop(L,1);
+}
+
+//--------------------------------------------------------------------------
+
+static const char * load_config = "\
+	local result = {}\n\
+	local function getenv(name) return assert(os.getenv(name), [[os.getenv() failed: ]] .. name) end\n\
+	local sep = package.config:sub(1,1)\n\
+	local current_path = [[.]]..sep\n\
+	local function include(filename)\n\
+		local last_path = current_path\n\
+		local path, name = filename:match([[(.*]]..sep..[[)(.*)$]])\n\
+		if path then\n\
+			if path:sub(1,1) == sep then	-- root\n\
+				current_path = path\n\
+			else\n\
+				current_path = current_path .. path\n\
+			end\n\
+		else\n\
+			name = filename\n\
+		end\n\
+		local f = assert(io.open(current_path .. name))\n\
+		local code = assert(f:read [[*a]])\n\
+		code = string.gsub(code, [[%$([%w_%d]+)]], getenv)\n\
+		f:close()\n\
+		assert(load(code,[[@]]..filename,[[t]],result))()\n\
+		current_path = last_path\n\
+	end\n\
+	setmetatable(result, { __index = { include = include } })\n\
+	local config_name = ...\n\
+	include(config_name)\n\
+	setmetatable(result, nil)\n\
+	return result\n\
+";
+
+//--------------------------------------------------------------------------
+
+int
+main(int argc, char *argv[])
+ {
+	const char * config_file = NULL ;
+	if (argc > 1)
+    {
+		config_file = argv[1];
+	} else {
+		fprintf(stderr, "Need a config file. Please read skynet wiki : https://github.com/cloudwu/skynet/wiki/Config\n""usage: skynet configfilename\n");
+		return 1;
+	}
+
+    skynet_env_init();
+
+	struct skynet_config config;
+
+	struct lua_State *L = luaL_newstate();
+	luaL_openlibs(L);	// link lua lib
+
+	int err =  luaL_loadbufferx(L, load_config, strlen(load_config), "=[skynet config]", "t");
+	assert(err == LUA_OK);
+	lua_pushstring(L, config_file);
+
+	err = lua_pcall(L, 1, 1, 0);
+	if (err)
+    {
+		fprintf(stderr,"%s\n",lua_tostring(L,-1));
+		lua_close(L);
+		return 1;
+	}
+    
+    _init_env(L);
+    lua_close(L);
+    
+	config.thread =  optint("thread",8);
+	config.module_path = optstring("cpath","./cservice/?.so");
+	config.harbor = optint("harbor", 1);
+	config.bootstrap = optstring("bootstrap","snlua bootstrap");
+	config.daemon = optstring("daemon", NULL);
+	config.logger = optstring("logger", NULL);
+	config.logservice = optstring("logservice", "logger");
+	config.profile = optboolean("profile", 1);
+
+	
+
+    printf("struct skynet_config - thread:%d,harbor:%d,profile:%d\n",config.thread,config.harbor,config.profile);
+    printf("struct skynet_config - daemon:%s\n",config.daemon);
+    printf("struct skynet_config - module_path:%s\n",config.module_path);
+    printf("struct skynet_config - bootstrap:%s\n",config.bootstrap);
+    printf("struct skynet_config - logger:%s\n",config.logger);
+    printf("struct skynet_config - logservice:%s\n",config.logservice);
+
+    const char * standalone = optstring("standalone", NULL);
+    printf("struct skynet_config - standalone:%s\n",standalone);
+}
+
+
+//--------------------------------------------------------------------------

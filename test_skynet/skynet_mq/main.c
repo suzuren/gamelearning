93ed2ec5295991bf7a5d99a769cb4e0ee1f9eb31
[szren] <[2528751462@qq.com]>
2018-09-07 11:38:24 +0800

    test_skynet

--- test_skynet/skynet_mq/main.c
+++ test_skynet/skynet_mq/main.c
@@ -25,8 +25,37 @@ drop_message(struct skynet_message *msg, void *ud) {
 
 //--------------------------------------------------------------------------
 
+
+void tets_message_size_and_type()
+{
+	int len = 1024;
+	int type = PTYPE_SOCKET;
+	size_t data_info = len | (size_t)type << MESSAGE_TYPE_SHIFT;
+
+	int de_type = data_info >> MESSAGE_TYPE_SHIFT;
+	size_t de_size = data_info & MESSAGE_TYPE_MASK;
+
+	printf("tets_message_size_and_type - size_t:%ld,MESSAGE_TYPE_SHIFT:%ld,MESSAGE_TYPE_MASK:%ld,len:%d,type:%d,data_info:%ld - de_size:%ld,de_type:%d\n",sizeof(size_t), MESSAGE_TYPE_SHIFT, MESSAGE_TYPE_MASK,len,type, data_info, de_size, de_type);
+
+	// tets_message_size_and_type - size_t:8,MESSAGE_TYPE_SHIFT:56,MESSAGE_TYPE_MASK:72057594037927935,len:1024,type:6,data_info:432345564227568640 - de_size:1024,de_type:6
+	// 72057594037927935  -> 0000 0000 ‭1111 1111   1111 1111   1111 1111   1111 1111   1111 1111   1111 1111   1111 1111‬
+    //‭ 432345564227568640 -> 0000 0110 0000 0000   0000 0000   0000 0000   0000 0000   0000 0000   0000 0100   0000 0000‬
+	// ---------------------------------------------------------------------------------------------------------------------
+	//                    &  0000 0000 0000 0000   0000 0000   0000 0000   0000 0000   0000 0000   0000 0100   0000 0000‬
+	// ---------------------------------------------------------------------------------------------------------------------
+	//                    ==                                                                             100   0000 0000‬
+	// ---------------------------------------------------------------------------------------------------------------------
+	//                    ==                                                                             十进制的 1024
+
+
+	// 432345564227568640 -> 0000 0110 0000 0000   0000 0000   0000 0000   0000 0000   0000 0000   0000 0100   0000 0000‬
+	// 右移 56 位         -> 0000 0110 == 十进制的 6
+
+}
+
 int main(int argc, char *argv[])
 {
+	tets_message_size_and_type();
     printf("test skynet message queue.\n");
 
 	skynet_mq_init();

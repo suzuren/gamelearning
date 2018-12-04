401db87391944651c970d1612f653fc13ff9e2b1
sucer <root@sucer.pc>
2018-12-04 14:39:31 +0800

    userright

--- test/helloworld.cpp
+++ test/helloworld.cpp
@@ -5,24 +5,25 @@
 
 #include "test_helloworld.h"
 #include "test_socket_server.h"
+#include "test_userright.h"
 
 int main(int argc, const char** argv)
 {
 	printf("hello test!\n");
 
-	double d1 = 0.01;
-	double d2 = 0.001;
-	double d3 = 0.0001;
-	printf("d1:%g\n", d1);
-	printf("d2:%g\n", d2);
-	printf("d3:%g\n", d3);
+	//double d1 = 0.01;
+	//double d2 = 0.001;
+	//double d3 = 0.0001;
+	//printf("d1:%g\n", d1);
+	//printf("d2:%g\n", d2);
+	//printf("d3:%g\n", d3);
 
 	//stGangCardResult GangCardResult;
 	//tets_memory_zero(GangCardResult);
 	//func1();
 
-	test_socket_server();
-
+	//test_socket_server();
+	testFuncUserRight();
 
 
 	return 0;

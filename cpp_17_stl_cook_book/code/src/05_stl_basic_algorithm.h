0cfa817f1e234b354a4f825856764870fab7b9c9
sucer <root@sucer.pc>
2019-01-19 18:54:14 +0800

    stl

--- cpp_17_stl_cook_book/code/src/05_stl_basic_algorithm.h
+++ cpp_17_stl_cook_book/code/src/05_stl_basic_algorithm.h
@@ -10,6 +10,7 @@
 #include <algorithm>
 //#include<bits/stdc++.h>
 #include <random>
+#include <sstream>
 
 //--------------------------------------------------------------------------------
 
@@ -27,6 +28,189 @@ static void print(const std::vector<int> &v)
 {
 	std::copy(std::begin(v), std::end(v), std::ostream_iterator<int>{std::cout, ", "});
 	std::cout << '\n';
+}
+
+//--------------------------------------------------------------------------------
+
+struct city
+{
+	std::string name;
+	unsigned population;
+};
+
+bool operator==(const city &a, const city &b)
+{
+	return a.name == b.name && a.population == b.population;
+}
+
+std::ostream& operator<<(std::ostream &os, const city &city)
+{
+	return os << "{" << city.name << ", " << city.population << "}";
+}
+
+template <typename C>
+static auto opt_print(const C &container)
+{
+	return[end_it(std::end(container))](const auto &item)
+	{
+		if (item != end_it)
+		{
+			std::cout << *item << '\n';
+		}
+		else
+		{
+			std::cout << "<end>\n";
+		}
+	};
+}
+
+void stl_finding_items()
+{
+	const std::vector<city> c{
+		{ "Aachen", 246000 },
+		{ "Berlin", 3502000 },
+		{ "Braunschweig", 251000 },
+		{ "Cologne", 1060000 }
+	};
+
+	//std::cout << "stl_finding_items - ";
+	auto print_city(opt_print(c));
+	auto found_cologne(std::find(std::begin(c), std::end(c),city{ "Cologne", 1060000 }));
+	std::cout << "stl_finding_items - ";
+	print_city(found_cologne);
+
+	std::cout << "stl_finding_items - ";
+	auto found_cologne_2(std::find_if(std::begin(c), std::end(c),[](const auto &item) {return item.name == "Aachen";}));
+	print_city(found_cologne_2);
+
+
+	auto population_more_than([](unsigned i)
+	{		return [=](const city &item)  // [=] 按值捕获 i ，item 是 find_if 循环的时候传递进去的
+			{
+				return item.population > i;
+			};
+	});
+
+	auto found_large(std::find_if(std::begin(c), std::end(c),population_more_than(2000000)));
+	std::cout << "stl_finding_items - ";
+	print_city(found_large);
+
+
+	const std::vector<int> v{ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	auto print_int(opt_print(v));
+	// 对于要成功的 std::binary_search ，范围 [first, last) 必须至少相对于 value 部分有序
+	bool contains_7{ std::binary_search(std::begin(v), std::end(v), 7) };
+	std::cout << "stl_finding_items - contains_7 - ";
+	std::cout << contains_7 << '\n';
+
+	// std::equal_range  。其不会返回对应元素的迭代器给我们，不过会返回一组迭代器。
+	//第一个迭代器是指向第一个不小于给定值的元素。第二个迭代器指向第一个大于给定值
+	//的元素。我们的范围为数字1到10，那么第一个迭代器将指向7，因为其是第一个不小于7
+	//的元素。第二个迭代器指向8，因为其实第一个大于7的元素
+	auto[lower_it, upper_it](std::equal_range(std::begin(v), std::end(v), 7));
+	std::cout << "stl_finding_items - lower_it - ";
+	print_int(lower_it);
+	std::cout << "stl_finding_items - upper_it - ";
+	print_int(upper_it);
+	
+	//lower_bound  函数只会返回第一个迭代器，	而 upper_bound  则会返回第二个迭代器
+	std::cout << "stl_finding_items - lower_it - lower_it - ";
+	print_int(std::lower_bound(begin(v), end(v), 7));
+
+	std::cout << "stl_finding_items - upper_it - upper_it - ";
+	print_int(std::upper_bound(begin(v), end(v), 7));
+
+	//stl_finding_items - {Cologne, 1060000}
+	//stl_finding_items - {Aachen, 246000}
+	//stl_finding_items - {Berlin, 3502000}
+	//stl_finding_items - contains_7 - 1
+	//	stl_finding_items - lower_it - 7
+	//	stl_finding_items - upper_it - 8
+	//	stl_finding_items - lower_it - lower_it - 7
+	//	stl_finding_items - upper_it - upper_it - 8
+
+
+}
+//--------------------------------------------------------------------------------
+
+static auto norm(int min, int max, int new_max)
+{
+	const double diff(max - min);
+	return [=](int val)
+	{
+		return int((val - min) / diff * new_max);
+	};
+}
+
+static auto clampval(int min, int max)
+{
+	return [=](int val) -> int
+	{
+		return std::clamp(val, min, max);
+	};
+}
+
+void stl_reducing_range_in_vector()
+{
+	std::vector<int> v{ 0, 1000, 5, 250, 300, 800, 900, 321 };
+
+	// std::minmax_element  函数将帮助我们获得这两个值 - 最大值和最小值
+	const auto[min_it, max_it](std::minmax_element(std::begin(v), std::end(v)));
+	std::vector<int> v_norm;
+	v_norm.reserve(v.size());
+	//使用 std::transform  从第一个 vector  拷贝到第二个 vector
+	std::transform(std::begin(v), std::end(v), std::back_inserter(v_norm),norm(*min_it, *max_it, 255));
+
+	std::cout << "stl_reducing_range_in_vector - ";
+	std::copy(std::begin(v_norm), std::end(v_norm),	std::ostream_iterator<int>{std::cout, ", "});
+	std::cout << '\n';
+	std::transform(std::begin(v), std::end(v), std::begin(v_norm),clampval(0, 255));
+
+	std::cout << "stl_reducing_range_in_vector - ";
+	std::copy(std::begin(v_norm), std::end(v_norm), std::ostream_iterator<int>{std::cout, ", "});
+	std::cout << '\n';
+
+	//stl_reducing_range_in_vector - 0, 255, 1, 63, 76, 204, 229, 81, 
+	//stl_reducing_range_in_vector - 0, 255, 5, 250, 255, 255, 255, 255,
+
+
+}
+
+//--------------------------------------------------------------------------------
+
+template <typename Itr>
+static void print(Itr it, size_t chars)
+{
+	std::copy_n(it, chars, std::ostream_iterator<char>{std::cout});
+	std::cout << '\n';
+}
+
+void stl_pattern_search_string()
+{
+	const std::string long_string{
+		"Lorem ipsum dolor sit amet, consetetur"
+		" sadipscing elitr, sed diam nonumy eirmod" };
+	const std::string needle{ "elitr" };
+
+	auto match(std::search(std::begin(long_string), std::end(long_string), std::begin(needle), std::end(needle)));
+	std::cout << "stl_pattern_search_string - match_1 -";
+	print(match, 5);
+	std::cout << "stl_pattern_search_string - match_2 -";
+	auto match_2(std::search(std::begin(long_string), std::end(long_string), std::default_searcher(std::begin(needle), std::end(needle))));
+	print(match_2, 5);
+	std::cout << "stl_pattern_search_string - match_3 -";
+	auto match_3(std::search(std::begin(long_string), std::end(long_string), std::boyer_moore_searcher(std::begin(needle), std::end(needle))));
+	print(match_3, 5);
+
+	std::cout << "stl_pattern_search_string - match_4 -";
+	auto match_4(std::search(std::begin(long_string), std::end(long_string), std::boyer_moore_horspool_searcher(std::begin(needle), std::end(needle))));
+	print(match_4, 5);
+
+	//stl_pattern_search_string - match_1 -elitr
+	//stl_pattern_search_string - match_2 - elitr
+	//stl_pattern_search_string - match_3 - elitr
+	//stl_pattern_search_string - match_4 - elitr
+
 }
 
 //--------------------------------------------------------------------------------
@@ -117,6 +301,7 @@ int test_stl_basic_algorithm()
 	std::cout << "test_stl_basic_algorithm - sorting_containers -     print:";
 	print(vecRand);
 
+	//04
 	//-------------------------------------------------------------
 
 
@@ -136,6 +321,7 @@ int test_stl_basic_algorithm()
 	}
 	std::cout << '\n';
 	
+	//05
 	//-------------------------------------------------------------
 
 	//std::replace 使用这个函数将所有4替换成 0。
@@ -163,11 +349,70 @@ int test_stl_basic_algorithm()
 	std::cout << "removing_items_from_containers -    v3:";
 	print(v3);
 
+	//06
+	//-------------------------------------------------------------
+	std::cout << "transforming_items_in_containers vTran:";
+	// std::transform  函数工作原理和 std::copy  差不多，不过在拷贝的过程中会对源容器中的元素进行变换，这个变换函数由用户提供。
+	std::vector<int> vevTran{ 1, 2, 3, 4, 5 };
+	std::transform(std::begin(vevTran), std::end(vevTran), std::ostream_iterator<int>{std::cout, ", "}, [](int i) { return i * i; });
+	std::cout << '\n';
+	auto int_to_string([](int i)
+	{
+		std::stringstream ss;
+		ss << i << "^2 = " << i * i;
+		return ss.str();
+	});
+	std::vector<std::string> vs;
+	std::transform(std::begin(vevTran), std::end(vevTran), std::back_inserter(vs),int_to_string);
+	std::cout << "transforming_items_in_containers -  vs:\n";
+	std::copy(std::begin(vs), std::end(vs), std::ostream_iterator<std::string>{std::cout, "\n"});
+
+	/*
+test_stl_basic_algorithm - copying_items m(1, one), (2, two), (3, three), 
+test_stl_basic_algorithm - copying_items v(1, one), (2, two), (3, three), (4, four), (5, five), 
+test_stl_basic_algorithm - copying_items m(1, one), (2, two), (3, three), (4, four), (5, five), 
+test_stl_basic_algorithm - copying_items v(1, ), (2, ), (3, ), (4, ), (5, ), 
+test_stl_basic_algorithm - sorting_containers - is_sorted:1
+test_stl_basic_algorithm - sorting_containers - is_sorted:0
+test_stl_basic_algorithm - sorting_containers -     print:5, 3, 8, 9, 6, 2, 7, 4, 1, 10, 
+test_stl_basic_algorithm - sorting_containers - is_sorted:1
+test_stl_basic_algorithm - sorting_containers -     print:1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 
+test_stl_basic_algorithm - sorting_containers -     print:3, 4, 1, 2, 8, 9, 7, 10, 6, 5, 
+test_stl_basic_algorithm - sorting_containers -     print:1, 2, 3, 4, 5, 10, 8, 7, 9, 6, 
+test_stl_basic_algorithm - sorting_containers -        mv:{-10, 20} {1, 50} {3, 70} {5, 100} {-123, 1000} 
+removing_items_from_containers - print:1, 2, 4, 4, 5, 4, 7, 8, 9, 4, 
+removing_items_from_containers - print:1, 2, 0, 0, 5, 0, 7, 8, 9, 0, 
+removing_items_from_containers - print:1, 2, 0, 0, 5, 0, 7, 8, 9, 0, 
+removing_items_from_containers -    v2:2, 0, 0, 0, 8, 0, 
+removing_items_from_containers -    v3:2, 0, 0, 0, 8, 0, 
+transforming_items_in_containers vTran:1, 4, 9, 16, 25, 
+transforming_items_in_containers -  vs:
+1^2 = 1
+2^2 = 4
+3^2 = 9
+4^2 = 16
+5^2 = 25
+	*/
+
+	//07 finding_items
+	//-------------------------------------------------------------
+	//std::cout << "finding_items:";
+	stl_finding_items();
+
+	//-------------------------------------------------------------
+	// 08
+	stl_reducing_range_in_vector();
+
+	//-------------------------------------------------------------
+	// 09
+	stl_pattern_search_string();
 
 	//-------------------------------------------------------------
 	std::vector<std::pair<int, int>> vecType;
 	std::pair<int, int> temp{ 1 ,2 };
 	vecType.emplace_back(temp);
+	vecType.emplace_back(3, 4);
+
 
 	return 0;
 }
@@ -216,5 +461,30 @@ std::partition
 范围的前端。剩下的将放在范围的后方。
 
 
+
+算法函数 作用
+
+std::find
+可将一个搜索范围和一个值作为参数。函数将返回找到的第一个值
+的迭代器。线性查找。
+
+std::find_if 与std::find原理类似，不过其使用谓词函数替换比较值。
+
+std::binary_search
+可将一个搜索范围和一个值作为参数。执行二分查找，当找到对应
+元素时，返回true；否则，返回false。
+
+std::lower_bound
+可将一个查找返回和一个值作为参数，并且执行二分查找，返回第
+一个不小于给定值元素的迭代器。
+
+std::upper_bound
+与std::lower_bound类似，不过会返回第一个大于给定值元素的迭
+代器。
+
+std::equal_range
+可将一个搜索范围和一个值作为参数，并且返回一对迭代器。其第
+一个迭代器和std::lower_bound返回结果一样，第二个迭代器和
+std::upper_bound返回结果一样。
 
 */

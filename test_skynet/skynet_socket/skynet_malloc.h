07410f908b7936bf474be1df9ef0767781185b73
szren <2528751462@qq.com>
2019-06-25 00:58:23 +0800

    skynet_socket

---
+++ test_skynet/skynet_socket/skynet_malloc.h
@@ -0,0 +1,26 @@
+#ifndef skynet_malloc_h
+#define skynet_malloc_h
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h> 
+
+#define skynet_malloc malloc
+#define skynet_free free
+
+void * skynet_realloc(void *ptr, size_t size)
+{
+	if (ptr == NULL) return skynet_malloc(size);
+	void* rawptr = ptr;
+	void *newptr = realloc(rawptr, size);
+	return newptr;
+}
+
+void * skynet_copymem(void *des_ptr, size_t des_size, void *src_buff, size_t src_size)
+{
+	void *newptr = skynet_realloc(des_ptr, src_size);
+	memcpy(newptr + des_size, src_buff, src_size);
+	return newptr;
+}
+
+#endif

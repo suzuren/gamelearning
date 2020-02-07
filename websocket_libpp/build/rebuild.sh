981946c4c9b2805d3dd54ae626f659273806db71
szren <2528751462@qq.com>
2020-02-08 07:33:57 +0800

    websocketpp

---
+++ websocket_libpp/build/rebuild.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+rm -rf CMakeFiles CMakeCache.txt cmake_install.cmake Makefile
+
+cmake ./
+while getopts rR opt
+do
+	case $opt in
+		r)
+			make clean
+			;;
+		R)
+			make clean
+			;;
+		*)
+			;;
+	esac
+done
+make -j 1
